7a48259f21d1fa3c15170faa68d2e90ef7a6feac
Jonathan Fielder <50757815+jonathanFielder@users.noreply.github.com>
2021-03-29 11:17:37 -0400

    Deleting directory to reupload with one less folder

--- reverb-price-finder/reverb-price-finder.py
+++
@@ -1,9 +0,0 @@
-
-from reverb_price_finder.__main__ import main
-
-
-
-if __name__ == '__main__':
-    main()
-
-
--- reverb-price-finder/scheduler.py
+++
@@ -1,6 +0,0 @@
-import reverb_price_finder.auto_schedule as schedule
-print('Waiting to run scheduled Reverb Searches.')
-print('Downsize me.')
-
-
-schedule.main()
